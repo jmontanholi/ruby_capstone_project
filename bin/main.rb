50c57dd1b3411904a3d6b446d0fc87633f6de991
jmontanholi <jonunes9326@hotmail.com.br>
2021-04-06 17:53:45 -0300

    Start user interface in main.rb

--- bin/main.rb
+++ bin/main.rb
@@ -2,3 +2,20 @@ require 'nokogiri'
 require 'httparty'
 require 'pry'
 require 'rainbow'
+
+def greeter 
+  puts 'Welcome to the Microsoft Store Scraper. To start press enter.'
+  gets.chomp
+end
+
+def search 
+  puts 'Please insert the name of the game you want to search for.'
+  game_name = gets.chomp.to_s
+  until game_name.match(/\w{3,}/i)
+    puts "Please enter a valid name"
+    game_name = gets.chomp.to_s
+  end
+end
+
+greeter
+search
