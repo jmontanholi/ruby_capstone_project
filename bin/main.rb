0ae66da8f9f02d708ca1037da9863300af329a16
jmontanholi <jonunes9326@hotmail.com.br>
2021-04-06 18:11:29 -0300

    Change user interface colors on main.rb

--- bin/main.rb
+++ bin/main.rb
@@ -5,12 +5,12 @@ require 'rainbow'
 require './lib/scrap_logic.rb'
 
 def greeter 
-  puts Rainbow('Welcome to the Microsoft Store Scraper. To start press enter.').magenta
+  puts Rainbow('Welcome to the Microsoft Store Scraper. To start press enter.').cyan
   gets.chomp
 end
 
 def search 
-  puts Rainbow('Please insert the name of the game you want to search for. Please use no accents, e.g. "~"').magenta
+  puts Rainbow('Please insert the name of the game you want to search for. Please use no accents, e.g. "~"').cyan
   game_name = gets.chomp.to_s
   until game_name.match(/\w{3,}/i)
     puts Rainbow('Please enter a valid name with no accents, e.g. "~"').red
