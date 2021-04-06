fd914e51053fb8e367a33db47750ec9870398c59
jmontanholi <jonunes9326@hotmail.com.br>
2021-04-06 12:35:39 -0300

    Add class Scraper to scrap_logic

--- lib/scrap_logic.rb
+++ lib/scrap_logic.rb
@@ -1,2 +1,20 @@
 require 'nokogiri'
 require 'httparty'
+
+class Scraper 
+  attr_reader :url, :game_name
+
+  def initialize(game_name)
+    @game_name = game_name
+    @url = "https://www.microsoft.com/en-us/search/shop/games?q=#{game_name}"
+  end
+
+  def parsing(url)
+    unparsed_url = HTTParty.get(url)
+    Nokogiri::HTML(unparsed_url)
+  end
+
+  def scrape
+    parsed_page = parsing(@url)
+  end
+end
