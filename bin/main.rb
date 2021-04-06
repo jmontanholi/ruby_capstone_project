b79e0bf7a45e85aba28c02dccb85f79da1d20417
jmontanholi <jonunes9326@hotmail.com.br>
2021-04-06 16:43:20 -0300

    Add colors to the results

--- bin/main.rb
+++ bin/main.rb
@@ -1,3 +1,4 @@
 require 'nokogiri'
-require 'open-uri'
+require 'httparty'
 require 'pry'
+require 'rainbow'
--- lib/scrap_logic.rb
+++ lib/scrap_logic.rb
@@ -1,38 +1,44 @@
 require 'nokogiri'
-require 'open-uri'
+require 'httparty'
 require 'pry'
+require 'rainbow'
 
-class Scraper 
+class Scraper
   attr_reader :url, :game_name
 
   def initialize(game_name)
     @game_name = game_name
-    @url = "https://www.microsoft.com/en-us/search/shop/games?q=#{game_name.gsub(' ', "%20")}"
+    @url = "https://www.microsoft.com/en-us/search/shop/games?q=#{game_name.gsub(' ', '%20')}"
   end
 
   def parsing(url)
-    Nokogiri::HTML(URI.open(url))
+    page = HTTParty.get(url)
+    Nokogiri::HTML(page.body)
   end
 
   def scrape
     parsed_page = parsing(@url)
-    games = parsed_page.css('h3.c-subheading-6')
-    results_number = games.count
+    games = parsed_page.css('div.m-channel-placement-item')
 
     create_game_hash(games)
   end
 
   def create_game_hash(games)
-    parsed_page = parsing(@url)
-    games = parsed_page.css('div.m-channel-placement-item')
     games.map do |game|
       begin
-        puts "\nName: #{game.css('h3.c-subheading-6').children[0].text}\nPrice: #{game.css('div.c-price').children[1].children[0].text.gsub(' ', '').gsub("\r", '').gsub("\n",'')}\nLink: https://www.microsoft.com#{game.css('a').attr('href').value}"
-      rescue 
-        puts "\nName: #{game.css('h3.c-subheading-6').children[0].text}\nPrice: Nothing specified, check link for more options\nLink: https://www.microsoft.com#{game.css('a').attr('href').value}"
-      
+        puts Rainbow("\nName: ").cyan + Rainbow(game.css('h3.c-subheading-6').children[0].text.to_s).green
+        puts Rainbow('Price: ').cyan +
+             Rainbow(game.css('div.c-price').children[1].children[0].text.gsub(' ', '').gsub("\r", '').gsub("\n",
+                                                                                                            '')).green
+        puts Rainbow('Link: ').cyan + Rainbow("https://www.microsoft.com#{game.css('a').attr('href').value}").green
+      rescue StandardError
+        puts Rainbow("\nName: ").cyan + Rainbow(game.css('h3.c-subheading-6').children[0].text.to_s).green
+        puts Rainbow('Price: ').cyan + Rainbow('Nothing specified, check link for more options').green
+        puts Rainbow('Link: ').cyan + Rainbow("https://www.microsoft.com#{game.css('a').attr('href').value}").green
       end
     end
   end
-
 end
+
+test = Scraper.new('call of duty')
+test.scrape
