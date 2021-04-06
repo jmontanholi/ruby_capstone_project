aed247750caa6056364220bfefea03a855ca0e14
jmontanholi <jonunes9326@hotmail.com.br>
2021-04-06 18:05:08 -0300

    Change text that asks for input

--- bin/main.rb
+++ bin/main.rb
@@ -5,15 +5,15 @@ require 'rainbow'
 require './lib/scrap_logic.rb'
 
 def greeter 
-  puts 'Welcome to the Microsoft Store Scraper. To start press enter.'
+  puts Rainbow('Welcome to the Microsoft Store Scraper. To start press enter.').magenta
   gets.chomp
 end
 
 def search 
-  puts 'Please insert the name of the game you want to search for.'
+  puts Rainbow('Please insert the name of the game you want to search for. Please use no accents, e.g. "~"').magenta
   game_name = gets.chomp.to_s
   until game_name.match(/\w{3,}/i)
-    puts "Please enter a valid name"
+    puts Rainbow('Please enter a valid name with no accents, e.g. "~"').red
     game_name = gets.chomp.to_s
   end
 
