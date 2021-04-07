ce513f82211f5c2d38eb70a87296fb0ab0a1f809
jmontanholi <jonunes9326@hotmail.com.br>
2021-04-07 11:30:45 -0300

    Add loop for multiple searchs

--- bin/main.rb
+++ bin/main.rb
@@ -10,13 +10,18 @@ def greeter
 end
 greeter
 
-puts Rainbow('Please insert the name of the game you want to search for. Please use no accents, e.g. "~"').cyan
-game_name = gets.chomp.to_s
-if !game_name.ascii_only?
-  until game_name.ascii_only? && game_name.match(/\w{3,}/i)
-    puts Rainbow('Please enter a valid name with no accents, e.g. "~"').red
-    game_name = gets.chomp.to_s
+end_parameter = String.new
+until end_parameter == 'exit'
+  puts Rainbow('Please insert the name of the game you want to search for. Please use no accents, e.g. "~"').cyan
+  game_name = gets.chomp.to_s
+  if !game_name.ascii_only?
+    until game_name.ascii_only? && game_name.match(/\w{3,}/i)
+      puts Rainbow('Please enter a valid name with no accents, e.g. "~"').red
+      game_name = gets.chomp.to_s
+    end
   end
+  puts search(game_name)
+  puts Rainbow('If you want to search again press enter, if you want to exit please insert "exit"').cyan
+  end_parameter = gets.chomp.to_s.downcase
 end
 
-puts search(game_name)
