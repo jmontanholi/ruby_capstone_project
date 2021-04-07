e8937adfbf611857e174acca3811558f2d220478
jmontanholi <jonunes9326@hotmail.com.br>
2021-04-07 11:24:34 -0300

    Add invalid message for accents on words

--- bin/main.rb
+++ bin/main.rb
@@ -12,9 +12,11 @@ greeter
 
 puts Rainbow('Please insert the name of the game you want to search for. Please use no accents, e.g. "~"').cyan
 game_name = gets.chomp.to_s
-until game_name.match(/\w{3,}/i)
-  puts Rainbow('Please enter a valid name with no accents, e.g. "~"').red
-  game_name = gets.chomp.to_s
+if !game_name.ascii_only?
+  until game_name.ascii_only? && game_name.match(/\w{3,}/i)
+    puts Rainbow('Please enter a valid name with no accents, e.g. "~"').red
+    game_name = gets.chomp.to_s
+  end
 end
 
 puts search(game_name)
