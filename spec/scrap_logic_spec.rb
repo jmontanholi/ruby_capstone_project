179f8ddd9640866f94ae521a79f4e1480faab5f0
jmontanholi <jonunes9326@hotmail.com.br>
2021-04-07 11:35:17 -0300

    Fix rspec tests

--- spec/scrap_logic_spec.rb
+++ spec/scrap_logic_spec.rb
@@ -2,6 +2,7 @@ require './lib/scrap_logic'
 
 describe Scraper do
   test_object = Scraper.new('sekiro')
+  false_test = Scraper.new('kairo')
   describe '#scrape' do
     it 'returns an array of hashes containing the name, price and link of a game in microsoft store.' do
       expect(test_object.scrape).to eql(
@@ -19,10 +20,6 @@ describe Scraper do
          "GOTY Edition\e[0m\n\e[36mPrice: \e[0m\e[32m$59.99\e[0m\n\e[36mLink:" \
          " \e[0m\e[32mhttps://www.microsoft.com/en-us/p/sekiro-shadows-die-twice-goty-edition/bqd5wrrp2d6q\e[0m"]
       )
-    end
-
-    it 'returns an message to the user if the passed name is outside UTF-8' do 
-      expect(search('josé')).to eql('This is not a valid input')
     end
   end
 end
